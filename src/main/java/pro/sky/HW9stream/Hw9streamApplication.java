6c5b83bac94c2f18ef21cbc7ff4643b38d56dea8
MaraOD555 <ruffbc@hotmail.com>
2022-08-02 10:35:23 +0300

    Master commit

---
+++ src/main/java/pro/sky/HW9stream/Hw9streamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.HW9stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw9streamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw9streamApplication.class, args);
+	}
+
+}
